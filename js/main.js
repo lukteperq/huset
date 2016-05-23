8855ca810cac7443cc7d248a4c6a3f78c2a24b83
lukteperq <grimstadoyvind@gmail.com>
2016-05-23 14:02:56 +0200

    added desc to mainjs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function($){
+    /*NB: rekkefølgen MÅ stå i samvsar med rekkefølgen til checkboxene i "jobbe på huset", med nåværende kode  */
     var tooltips = [
-        'Vil du mikse drinker og ha det sosialt på jobb? Da kan du begynne å jobbe i baren. Drinkene er lette å lære og vi har enkle oppskrifter. Barsjefen er Stian Fenstad. ',
-        'Passer på at artistene har det bra. Lager kaffe, ordner mat og sørger for at artistene får en god opplevelse på «HUSET». Bookingansvarlig er Sara Løvf. ',
-        'Jobben i cover innebærer å ta i mot alle som kommer til «HUSET» med et smil, passe på jakkene deres og ta imot billetter og inngangspenger. Coversjefen heter Moa og er også sekretær i styret.',
-        'Dette er en gruppe som består av DJ-er, quizmastere og de som pynter «HUSET» til fest. Høres dette ut som noe for deg? Eventansvarlig er  Anette Brenden.',
-        'Ønsker du å bidra til å holde orden, bli kjent med masse nye folk og få gratis ordensvaktkurs bør du bli vakt på «HUSET». Vaktsjefen heter Robin Lein.',
-        'Liker du å lage plakater og andre grafiske ting, eller er du god til å ta bilder så bør du bli med i PR. PR/Info-ansvarlig er Mia Tollaksvik. ',
-        'Har ansvaret for at man hører og ser artistene. Dette er gruppa for deg som er glad i ledninger og rigging av utstyr!  Teknisk ansvarlig heter  Øyvind Aasen. ',
-        'Usikker på hva du har lyst til å jobbe med? Leder Jakob Fonstad eller nestleder Stian Brustad svarer på det du måtte lure på. '
+    /*Vakt*/        'Vil du mikse drinker og ha det sosialt på jobb? Da kan du begynne å jobbe i baren. Drinkene er lette å lære og vi har enkle oppskrifter. Barsjefen er Stian Fenstad. ',
+    /*Booking*/     'Passer på at artistene har det bra. Lager kaffe, ordner mat og sørger for at artistene får en god opplevelse på «HUSET». Bookingansvarlig er Sara Løvf. ',
+    /*Cover*/       'Jobben i cover innebærer å ta i mot alle som kommer til «HUSET» med et smil, passe på jakkene deres og ta imot billetter og inngangspenger. Coversjefen heter Moa og er også sekretær i styret.',
+    /*Event*/       'Dette er en gruppe som består av DJ-er, quizmastere og de som pynter «HUSET» til fest. Høres dette ut som noe for deg? Eventansvarlig er  Anette Brenden.',
+    /*Vakt*/        'Ønsker du å bidra til å holde orden, bli kjent med masse nye folk og få gratis ordensvaktkurs bør du bli vakt på «HUSET». Vaktsjefen heter Robin Lein.',
+    /*PR&Info*/     'Liker du å lage plakater og andre grafiske ting, eller er du god til å ta bilder så bør du bli med i PR. PR/Info-ansvarlig er Mia Tollaksvik. ',
+    /*Teknisk*/     'Har ansvaret for at man hører og ser artistene. Dette er gruppa for deg som er glad i ledninger og rigging av utstyr!  Teknisk ansvarlig heter  Øyvind Aasen. ',
+    /*Vet ikke*/    'Usikker på hva du har lyst til å jobbe med? Leder Jakob Fonstad eller nestleder Stian Brustad svarer på det du måtte lure på. '
     ];
   $(window).scroll(function(e){
         if($(this).scrollTop() > parseInt($('.site-branding').css('height'))){
