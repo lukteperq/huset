9165a1aa61d117ec8a7ebb8ebe67f2a560275f74
lukteperq <grimstadoyvind@gmail.com>
2016-05-25 17:15:33 +0200

    removed "written by" and footer skjiit
    asd

--- footer.php
+++ footer.php
@@ -18,9 +18,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo"> <!-- Styler site-footer pga bug -->
 		<?php get_sidebar('footer'); ?> <!-- Henter inn fra sidebar-footer -->
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'huset' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'huset' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'huset' ), 'huset', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
